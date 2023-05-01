c7ec9227cf19063a1036d478ea2f0f960f38fe67
Dmitry Tikhomirov <dmitry_tikhomirov@epam-qrqwc5f95m.home>
2023-05-01 23:55:29 +0100

    resources

---
+++ pyqt5 transfer/src/resources.py
@@ -0,0 +1,20 @@
+# Constant for ticket prices 
+ticket_price_adult = 10
+ticket_price_child = 5
+ticket_price_elderly = 5
+ticket_price_special = 0
+# Cimema hall size 
+cinema_rows = 10
+cinema_seats_per_row = 20
+# Program window size as % from screen
+main_window_height = 0.8 
+main_window_width = 0.8
+# Toolbar size as % from program window
+toolbar_height = 0.1
+# Strings
+main_window_title = "Collyers Theater Resevartion System"
+toolbar_button_names = ["Book Tickets", "Payment", "Manage Seats",  "Veiw Revenue", "Search Customer"]
+
+tickets_left_header = "Select shows"
+tickets_left_middle = "Select tickets"
+tickets_right_header = "Allocate seats"
